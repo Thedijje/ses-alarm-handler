28adb28bc09439f4dd3ba768caccecaacd99326b
dheeraj <dheeraj@cteinternational.net>
2019-09-16 15:05:08 +0530

    added phpunit for testing

---
+++ tests/unit/FirstTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    
+    public function multiplicationOfTwoNumber()
+
+    {
+        $this->assertTrue(true);
+    }
+
+
+}
